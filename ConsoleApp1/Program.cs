6498d86eff18d26e7970315551cc31a62472ecf9
s27498 <s27498@pjwstk.edu.pl>
2024-03-08 13:34:50 +0100

    Modification

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!!!!");
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAvg(arr));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
